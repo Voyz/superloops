b2991a60400f0d4e287d2b5e49a5a7cdcadcfa65
Sengolda <senushkabatawala@gmail.com>
2023-05-17 17:04:20 +0530

    Dont use f-strings for logging

--- bin/examples/02_events_01.py
+++ bin/examples/02_events_01.py
@@ -6,26 +6,28 @@ from superloops import SuperLoop
 def thread_name():
     return threading.current_thread().name
 
+
 class MyLoop(SuperLoop):
     def on_start(self):
-        print(f'on_start - {thread_name()}')
+        print(f"on_start - {thread_name()}")
         # Perform any necessary setup here
         return True  # Return False to prevent the loop from starting
 
     def on_stop(self):
-        print(f'on_stop - {thread_name()}')
+        print(f"on_stop - {thread_name()}")
         # Perform any necessary cleanup here
 
     def on_thread_start(self):
-        print(f'on_thread_start - {thread_name()}')
+        print(f"on_thread_start - {thread_name()}")
         # Perform any necessary setup here
 
     def on_thread_stop(self):
-        print(f'on_thread_stop - {thread_name()}')
+        print(f"on_thread_stop - {thread_name()}")
         # Perform any necessary cleanup here
 
     def cycle(self):
         pass
+
 
 loop = MyLoop()
 
--- bin/examples/04_multiple_threads.py
+++ bin/examples/04_multiple_threads.py
@@ -3,29 +3,32 @@ import time
 
 from superloops import SuperLoop, LoopController
 
-logging.basicConfig(level = logging.INFO)
+logging.basicConfig(level=logging.INFO)
 
-_LOGGER = logging.getLogger('multiple_threads')
+_LOGGER = logging.getLogger("multiple_threads")
+
 
 class GoodLoop(SuperLoop):
     def cycle(self):
         time.sleep(1)
-        _LOGGER.info(f'{self.thread_name} - processing')
+        _LOGGER.info(f"{self.thread_name} - processing")
+
 
 class BadLoop(SuperLoop):
     def cycle(self):
         time.sleep(1)
-        _LOGGER.info(f'{self.thread_name} - processing, failures={self._failures}')
+        _LOGGER.info(f"{self.thread_name} - processing, failures={self._failures}")
         self.failure()
+
 
 loop_controller = LoopController()
 good_loop = loop_controller.new_loop(GoodLoop())
-bad_loop = loop_controller.new_loop(BadLoop(max_loop_failures=3)) # will propagate its health after 3 failures
+bad_loop = loop_controller.new_loop(BadLoop(max_loop_failures=3))  # will propagate its health after 3 failures
 
 loop_controller.start()
 loop_controller.maintain_loops()
 
-_LOGGER.info('WAITING FOR FAILURE PROPAGATION')
+_LOGGER.info("WAITING FOR FAILURE PROPAGATION")
 
 # wait until the first failure propagation
 while loop_controller.green_light.is_set():
@@ -35,7 +38,7 @@ while loop_controller.green_light.is_set():
 while not loop_controller.green_light.is_set():
     time.sleep(0.1)
 
-_LOGGER.info('RESTART COMPLETE, SHUTTING DOWN')
+_LOGGER.info("RESTART COMPLETE, SHUTTING DOWN")
 
 loop_controller.stop()
 loop_controller.stop_loops()
